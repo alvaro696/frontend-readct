5768ff7816fecc7f45826ed32334938e2c4117c7
alvaro696 <alvaro00.am@outlook.com>
2025-01-15 18:29:54 -0400

    Practica 1

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
 import './App.css'
 import Home from './screens/Home'
 import Counter from './components/Counter/Counter.jsx'
+import Product from './screens/Product.jsx'
 
 const App = () => {
 
   return (
     <>
-      <Home />
-      <Counter />
+      {/* <Home />
+      <Counter /> */}
+      <Product />
     </>
   )
 }
