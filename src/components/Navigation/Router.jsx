e71e37b6e48285a396d10bb19639175aeff61895
alvaro696 <alvaro00.am@outlook.com>
2025-01-15 23:17:06 -0400

    clase 5

--- src/components/Navigation/Router.jsx
+++ src/components/Navigation/Router.jsx
@@ -4,6 +4,7 @@ import About from "../../screens/About";
 import Think from "../../screens/Think";
 import Counter from "../Counter/Counter";
 import { Route, Routes } from "react-router";
+import Login from "../../screens/Login";
 
 const Router = () => {
     return (
@@ -13,6 +14,7 @@ const Router = () => {
             <Route path="think" element={<Think />} />
             <Route path="about" element={<About />} />
             <Route path="counter" element={<Counter />} />
+            <Route path="login" element={<Login />} />
 
         </Routes>
     );
---
+++ src/screens/Login.jsx
@@ -0,0 +1,12 @@
+//import FormLogin from "../components/Forms/FormLogin";
+import FormLoginWithHook from "../components/Forms/FormLoginWithHook";
+import FormWithMotionAndHook from "../components/Forms/FormLoginWithMotion";
+
+const Login = () => {
+    return (
+        <>
+            <FormWithMotionAndHook titleForm={'Bienvenido'}/>
+        </>
+    );
+}
+export default Login;
